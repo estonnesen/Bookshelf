91b45576cb69201c73ed18320816a01bdd54e390
Erik Tonnesen <estonnesen@gmail.com>
2023-01-12 09:47:45 -0500

    changed overall look. fixed rendering redundancies so I don't need to render into html twice...

--- js/index.js
+++ js/index.js
@@ -8,52 +8,126 @@ class Bookshelf {
     }
   
     addBook(book) {
-      this.books.push(book)
+      this.books.unshift(book)
     }
-  
-    bookshelfRender() {
+
+    bookshelfRender() { // this function uses .map to scan through this.books array and render them to html when called upon
       const booksContainer = document.createElement('div');
       booksContainer.classList.add('books-container')
       body.append(booksContainer)
-    
-      this.books.map((eachBook)=>{
-        // const newBook = eachBook.renderBook()
-        // booksContainer.append(newBook)
-  
+
+      this.books.map((eachBook)=>{ 
         const newContainer = document.createElement('ul')
         newContainer.classList.add('new-container')
         booksContainer.append(newContainer)
+
+        // Create comment button for each new div that's rendered
+          const addComment = () => {
+              const commentBtn = document.createElement('button')
+              const commentDiv = document.createElement('div')
+              commentDiv.classList.add('comments')
+              commentBtn.classList.add('commentBtn')
+              commentBtn.innerText = "Comment"
+              commentDiv.append(commentBtn)
+              newContainer.appendChild(commentDiv)
+
+              commentBtn.addEventListener('click', () => {
+                  const inputField = document.createElement('input')
+                  inputField.classList.add('comment-input')
+                  inputField.setAttribute("placeholder", "Comment Here and press ENTER")
+                  inputField.setAttribute("maxlength", 280)
+                  commentDiv.append(inputField)
+                //   console.log(inputField)
+
+                  inputField.addEventListener('keypress', (e) => {
+                      if (e.key === "Enter") {
+                          const inputText = document.createElement('p')
+                          inputText.classList.add('inputText')
+                          inputText.innerText = inputField.value
+                          commentDiv.append(inputText)
+
+                          inputField.value = "" // sets input field empty again
+                        //   inputField.disabled = true
+                      }
+                  })
+
+                  commentBtn.disabled = true // this makes it so you can't keep adding input fields (googled this)
+              })
+          }
+        
+        addComment()
+
   
         const authorLi = document.createElement('li')
         authorLi.classList.add('author')
-        authorLi.innerText = eachBook.author
+        authorLi.innerText = `AUTHOR: ${eachBook.author}` 
         newContainer.appendChild(authorLi)
   
         const languageLi = document.createElement('li')
         languageLi.classList.add('language')
-        languageLi.innerText = eachBook.language
+        languageLi.innerText = `LANGUAGE: ${eachBook.language}`
         newContainer.appendChild(languageLi)
   
         const subjectLi = document.createElement('li')
         subjectLi.classList.add('subject')
-        subjectLi.innerText = eachBook.subject
+        subjectLi.innerText = `SUBJECT: ${eachBook.subject}`
         newContainer.appendChild(subjectLi)
   
         const titleLi = document.createElement('li')
         titleLi.classList.add('title')
-        titleLi.innerText = eachBook.title
+        titleLi.innerText = `TITLE: ${eachBook.title}`
         newContainer.appendChild(titleLi)
       })
-  
       return booksContainer
     }
 
-    bookshelfSort() {
+    addComment() {
+
+    }
+    
+    // Sorting functions. Not quite sure how to consolidate all this into one function yet...
+    authorSort() {
         this.books.sort((a, b)=>{
           if(a.author < b.author){
             return -1
           }
           if(a.author > b.author) {
+            return 1
+          }
+          return 0
+        })
+      }
+      
+    subjectSort() {
+        this.books.sort((a, b)=>{
+          if(a.subject < b.subject){
+            return -1
+          }
+          if(a.subject > b.subject) {
+            return 1
+          }
+          return 0
+        })
+      }
+
+    languageSort() {
+        this.books.sort((a, b)=>{
+          if(a.language < b.language){
+            return -1
+          }
+          if(a.language > b.language) {
+            return 1
+          }
+          return 0
+        })
+      }
+
+    titleSort() {
+        this.books.sort((a, b)=>{
+          if(a.title < b.title){
+            return -1
+          }
+          if(a.title > b.title) {
             return 1
           }
           return 0
@@ -67,38 +141,45 @@ class Bookshelf {
       this.language = language;
       this.subject = subject;
       this.title = title;
+    //   this.comment = []
     }
     
     // rendering a NEW book
-      renderBook(eachBook) {
-          const booksContainer = document.querySelector('.books-container'); // ref to master bookshelf list
+    // ******REDUNDANT*****
+    //   renderBook(eachBook) {
+    //       const booksContainer = document.querySelector('.books-container'); // ref to master bookshelf list
 
-          const newContainer = document.createElement('ul')
-          newContainer.classList.add('new-container')
-          booksContainer.prepend(newContainer)
+    //       const newContainer = document.createElement('ul')
+    //       newContainer.classList.add('new-container')
+    //       booksContainer.prepend(newContainer)
 
-          const authorLi = document.createElement('li')
-          authorLi.classList.add('author')
-          authorLi.innerText = eachBook.author
-          newContainer.appendChild(authorLi)
+    //       const commentBtn = document.createElement('button')
+    //       commentBtn.classList.add('commentBtn')
+    //       commentBtn.innerText = "Comment"
+    //       newContainer.appendChild(commentBtn)
 
-          const languageLi = document.createElement('li')
-          languageLi.classList.add('language')
-          languageLi.innerText = eachBook.language
-          newContainer.appendChild(languageLi)
+    //       const authorLi = document.createElement('li')
+    //       authorLi.classList.add('author')
+    //       authorLi.innerText = `Author: ${eachBook.author}`
+    //       newContainer.appendChild(authorLi)
 
-          const subjectLi = document.createElement('li')
-          subjectLi.classList.add('subject')
-          subjectLi.innerText = eachBook.subject
-          newContainer.appendChild(subjectLi)
+    //       const languageLi = document.createElement('li')
+    //       languageLi.classList.add('language')
+    //       languageLi.innerText = eachBook.language
+    //       newContainer.appendChild(languageLi)
 
-          const titleLi = document.createElement('li')
-          titleLi.classList.add('title')
-          titleLi.innerText = eachBook.title
-          newContainer.appendChild(titleLi)
+    //       const subjectLi = document.createElement('li')
+    //       subjectLi.classList.add('subject')
+    //       subjectLi.innerText = eachBook.subject
+    //       newContainer.appendChild(subjectLi)
 
-          return booksContainer;
-      }
+    //       const titleLi = document.createElement('li')
+    //       titleLi.classList.add('title')
+    //       titleLi.innerText = eachBook.title
+    //       newContainer.appendChild(titleLi)
+
+    //       return booksContainer;
+    //   }
     }
   
   
@@ -130,6 +211,8 @@ class Bookshelf {
   
   addBtn.addEventListener("click", function (event) {
     event.preventDefault();
+    const booksContainer = document.querySelector('.books-container')
+
     const newBook = new Book();
     const inputAuth = document.querySelector("#Author");
     newBook.author = inputAuth.value
@@ -139,10 +222,12 @@ class Bookshelf {
     newBook.subject = inputSubj.value
     const inputTitle = document.querySelector("#Title");
     newBook.title = inputTitle.value
-    
+
+    booksContainer.remove() // removes the original book list I rendered
     myBookshelf.addBook(newBook) // adds to the actual array of books
-    newBook.renderBook(newBook) // renders new book into html via DOM
+    // newBook.renderBook(newBook) // renders new book into html via DOM
     console.log(myBookshelf)
+    myBookshelf.bookshelfRender() // now we re-render the bookshelf
 
     // resets the form input box every time the button is clicked
     inputAuth.value = "";
@@ -151,18 +236,43 @@ class Bookshelf {
     inputTitle.value = "";
   });
 
-  const sortBtn = document.querySelector('#sort-button')
+// const sortBtn = document.querySelector('#sort-button') // used this in earlier versions to test sorting with a button
+
+const sortingSelection = document.getElementById("sort")
   
-sortBtn.addEventListener("click", function () {
+sortingSelection.addEventListener("change", function () {
     const booksContainer = document.querySelector('.books-container')
+    if(this.value === "makeSelect") {
+        console.log('Make a selection') // testing drop down each time...
+    }
+    if(this.value === "author") {
+        console.log('you selected author') // testing drop down each time...
+        myBookshelf.authorSort()
+    }
+    if(this.value === "subject") {
+        console.log('you selected subject')
+        myBookshelf.subjectSort()
+    }
+    if(this.value === "language") {
+        console.log('you selected language')
+        myBookshelf.languageSort()
+    }
+    if(this.value === "title") {
+        console.log('you selected title')
+        myBookshelf.titleSort()
+    }
+
     booksContainer.remove() // removes the original book list I rendered
-    // console.log(myBookshelf)
-    myBookshelf.bookshelfSort() // this sorts just the array of books including those that were added
     myBookshelf.bookshelfRender() // now we re-render the bookshelf
-});
+    console.log(myBookshelf)
+}, false);
+
+// commentBtn.addEventListener('click', ()=>{
+//     console.log('comment button')
+// })
 
 myBookshelf.bookshelfRender()
-  
+
   // // ***EVENTUALLY MAKE A MOUSEOVER TO DISPLAY ALL INFO FOR EACH BOOK****
   // // const bookImage = document.querySelector('li')
   
